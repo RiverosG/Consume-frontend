c4176a9206918310548dd473cec1e157d8ed75ee
gustavoriveros <20183160@aloe.ulima.edu.pe>
2024-11-10 15:34:59 -0500

    configurando proy

---
+++ src/Components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <>
+            <div className="Header">
+                <div className="container">
+                    
+                </div>
+            </div>
+        </>
+    )
+}
+export default Header
